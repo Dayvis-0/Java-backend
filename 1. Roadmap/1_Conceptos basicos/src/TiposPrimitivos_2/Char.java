21ffd75c815143986be4c616f4a2dad7e05800b2
Dayvis <amdayvis39@gmail.com>
2025-05-09 17:31:31 -0500

    09/05/2025 2

---
+++ 1. Roadmap/1_Conceptos basicos/src/TiposPrimitivos_2/Char.java
@@ -0,0 +1,19 @@
+package TiposPrimitivos_2;
+/*Unico caracter Unicode de 16bits
+Valor: 
+minimo -> \u0000 m
+maximo -> \uffff
+Permitidos
+'a'-'z' | '0' | '#', '%' | '\n', '\t'*/
+
+public class Char {
+    public static void main(String[] args) {
+        char letter = 'A';
+        char symbol = '#';
+        String unicode = "\u2764";
+        
+        System.out.println("Letra: " + letter);
+        System.out.println("Simbolo: " + symbol);
+        System.out.println("Unicode: " + unicode);
+    }
+}
---
+++ 1. Roadmap/1_Conceptos basicos/src/TiposPrimitivos_2/Float.java
@@ -0,0 +1,12 @@
+package TiposPrimitivos_2;
+/*32 bits
+Su rango esta mas alla del alcance de esta discusion
+Rango aproximado: ±3.4×10³⁸ (con ~6-7 cifras decimales de precisión)
+NO usar en: Calculos que requiere*/
+public class Float {
+    public static void main(String[] args) {
+        float num = 3.14f; // Sin el f, java lo interpreta como un dobuble
+        
+        System.out.println("Pi: " + num);
+    }
+}
