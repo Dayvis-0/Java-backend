509ff10cd61c934c4432e90f00602577e2edce71
dayvis <amdayvis39@gmail.com>
2025-06-24 15:44:02 -0500

    24/06/2025

---
+++ 1. Roadmap/2_LifecycleOfAProgram/1_CicloDeVidaDeUnPrograma/src/Muestra.java
@@ -0,0 +1,9 @@
+public class Muestra {
+    public static void main(String[] args) {
+        System.out.println("Hola desde java");
+    }
+    /*
+    javac convierte Muestra.java -> Muestra.class
+    java ejectuca Muestra.class con JVM
+    La JVM traduce el bytecode a codigo de maquina (0,1) para tu CPU*/
+}
---
+++ 1. Roadmap/3_DataTypes/1_Variables/src/VariablesEnJava_1/Variables.java
@@ -0,0 +1,10 @@
+package VariablesEnJava_1;
+/*Es una pieza de memoria que puede contener un valorr de datos. Una variable por lo tanto, tiene un 
+tipo de datos*/
+public class Variables {
+    public static void main(String[] args) {
+        /*Las variables generalmente se usan para almacenar informacion que su programa en Java necesita para hacer su trabajo*/
+        int number = 18;
+        number = nu
+    }
+}
