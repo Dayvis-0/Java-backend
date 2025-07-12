e0d1eea2e7649b9b76bdab01d6f881a31718ece3
dayvis <amdayvis39@gmail.com>
2025-07-11 21:52:02 -0500

    01/07/2025

---
+++ 1. Roadmap/3_DataTypes/1_Variables/src/DeclaracionDeVariables_3/LecturaDeVariable.java
@@ -0,0 +1,9 @@
+package DeclaracionDeVariables_3;
+
+public class LecturaDeVariable {
+    public static void main(String[] args) {
+        float myFloat = 199;
+        
+        System.out.println("My flotante: " + myFloat);
+    }
+}
